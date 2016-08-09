27d4fcb813ecab9694dabf36fc092d65f2090e07
ManjuMuralidharan <manju1695@gmail.com>
2016-08-09 19:00:38 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 
+			
+        Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		StringBuffer sb=new StringBuffer(s1);
+		
+		sb.reverse();
+		String s2=sb.toString();
+		System.out.println(s2);
+		
+
+
+
+			}
+
+}
